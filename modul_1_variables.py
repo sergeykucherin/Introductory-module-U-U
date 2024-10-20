7297dd359450951e52bdc5855ffb2509fd0f50b1
sergeykucherin <120263832+sergeykucherin@users.noreply.github.com>
2024-10-21 04:21:32 +0700

    Add files via upload

---
+++ modul_1_variables.py
@@ -0,0 +1,7 @@
+completed_homework_count = 12
+time_count = 1.5
+course_name = "Pyton"
+time_for_one_task = time_count / completed_homework_count
+print('Курс:',course_name,',', 'всего задач:',completed_homework_count,',',  'затрачено часов:',time_count,',',  'среднее время выполнения:',time_for_one_task,'часа.')
+
+
