ccfd60a273de6c09b2a7522bc437f480453ca147
sergeykucherin <120263832+sergeykucherin@users.noreply.github.com>
2024-10-27 13:52:36 +0700

    Delete modul_2_2.py

--- modul_2_2.py
+++
@@ -1,9 +0,0 @@
-first = input('Введите первое число: ')
-second = input('Введите второе число: ')
-third = input('Введите третье число: ')
-if first == second == third:
-    print(3)
-elif first == second != third or first != second == third:
-    print(2)
-elif first != second != third:
-    print(0)
