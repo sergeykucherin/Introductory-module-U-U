1daf62c5dda587363d75a1bb4b8b433ce79aec1d
sergeykucherin <120263832+sergeykucherin@users.noreply.github.com>
2024-12-27 09:30:39 +0700

    Add files via upload

---
+++ module_9_1_call_at_once.py
@@ -0,0 +1,24 @@
+
+def apply_all_func(int_list, *functions):
+    results = {}
+    for function in functions:
+        results [function.__name__] = function(int_list)
+    return results
+
+def min_list(int_list):
+    return min(int_list)
+
+def max_list(int_list):
+    return max(int_list)
+
+def len_list(int_list):
+    return len(int_list)
+
+def sum_list(int_list):
+    return sum(int_list)
+
+def sorted_list(int_list):
+    return sorted(int_list)
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
